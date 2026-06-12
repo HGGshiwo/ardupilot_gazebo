d7de49dfafb2d967bcda037838d0761dc6bbe9b4
HGGshiwo <3190102788@zju.edu.cn>
2026-06-12 22:21:17 +0800

    增加云台功能

--- src/GimbalSmall2dPlugin.cc
+++ src/GimbalSmall2dPlugin.cc
@@ -1,17 +1,18 @@
-#include <cmath>
-#include <mutex>
 #include <string>
-#include <thread>
 #include <vector>
+#include <mutex>
+#include <thread>
+#include <cmath>
 
-#include <gazebo/gazebo.hh>
-#include <gazebo/physics/physics.hh>
+#include "gazebo/common/PID.hh"
+#include "gazebo/physics/physics.hh"
+#include "gazebo/gazebo.hh"
 
 // 引入 ROS 核心及消息机制
-#include <ros/callback_queue.h>
 #include <ros/ros.h>
-#include <rsos_msgs/SetGimbalAngle.h>
+#include <ros/callback_queue.h>
 #include <std_msgs/Float64.h>
+#include <rsos_msgs/SetGimbalAngle.h> 
 
 using namespace gazebo;
 using namespace std;
@@ -20,9 +21,10 @@ using namespace std;
 
 GZ_REGISTER_MODEL_PLUGIN(GimbalSmall2dPlugin)
 
-class gazebo::GimbalSmall2dPluginPrivate {
-public:
-  ~GimbalSmall2dPluginPrivate() {
+class gazebo::GimbalSmall2dPluginPrivate
+{
+  public: ~GimbalSmall2dPluginPrivate()
+  {
     if (rosNode) {
       rosNode->shutdown();
     }
@@ -31,78 +33,53 @@ public:
     }
   }
 
-public:
-  void QueueThread();
+  public: void QueueThread();
+  public: bool OnServiceCall(rsos_msgs::SetGimbalAngle::Request &req,
+                              rsos_msgs::SetGimbalAngle::Response &res);
 
-public:
-  bool OnServiceCall(rsos_msgs::SetGimbalAngle::Request &req,
-                     rsos_msgs::SetGimbalAngle::Response &res);
-
-public:
-  std::vector<event::ConnectionPtr> connections;
-
-public:
-  physics::ModelPtr model;
-
-public:
-  physics::JointPtr tiltJoint;
-
-public:
-  common::Time lastUpdateTime;
+  public: std::vector<event::ConnectionPtr> connections;
+  public: physics::ModelPtr model;
+  public: physics::JointPtr tiltJoint;
+  
+  // 🌟 回归原版：使用原作者的独立 PID 控制器
+  public: common::PID pid;
+  public: common::Time lastUpdateTime;
 
   // --- ROS 核心变量 ---
-public:
-  unique_ptr<ros::NodeHandle> rosNode;
-
-public:
-  ros::ServiceServer rosSrv;
-
-public:
-  ros::Publisher rosPub;
-
-public:
-  ros::CallbackQueue rosQueue;
-
-public:
-  std::thread rosQueueThread;
-
-public:
-  std::mutex mutex;
+  public: unique_ptr<ros::NodeHandle> rosNode;
+  public: ros::ServiceServer rosSrv;
+  public: ros::Publisher rosPub;
+  public: ros::CallbackQueue rosQueue;
+  public: std::thread rosQueueThread;
+  public: std::mutex mutex;
 
   // --- 配置参数与状态控制 ---
-public:
-  std::string rosServiceName = "set_gimbal_angle";
-
-public:
-  std::string rosTopicName = "gimbal_pitch";
-
-public:
-  std::string jointName = "tilt_joint";
-
-public:
-  std::string jointNameScoped = ""; // 🌟 存储带作用域的关节名
-public:
-  std::string vehicleModelName = "";
-
-public:
-  std::string currentMode = "body";
-
-public:
-  double targetAngleDeg = 0.0;
-
-public:
-  common::Time lastPubTime;
+  public: std::string rosServiceName = "set_gimbal_angle";
+  public: std::string rosTopicName = "gimbal_pitch";
+  public: std::string jointName = "tilt_joint";
+  public: std::string vehicleModelName = ""; 
+  
+  public: std::string currentMode = "body"; 
+  // 🌟 默认角度设为向前的 0 度
+  public: double targetAngleDeg = 0.0;       
+  public: common::Time lastPubTime;
 };
 
 GimbalSmall2dPlugin::GimbalSmall2dPlugin()
-    : dataPtr(new GimbalSmall2dPluginPrivate) {}
+  : dataPtr(new GimbalSmall2dPluginPrivate)
+{
+  // 🌟 100% 还原原作者的 PID 参数和严格的 ±1.0 扭矩限幅
+  // Init(P, I, D, Imax, Imin, CmdMax, CmdMin)
+  this->dataPtr->pid.Init(1, 0, 0, 0, 0, 1.0, -1.0);
+}
 
-void GimbalSmall2dPlugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf) {
+void GimbalSmall2dPlugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf)
+{
   this->dataPtr->model = _model;
 
   if (_sdf->HasElement("joint"))
     this->dataPtr->jointName = _sdf->Get<std::string>("joint");
-
+    
   if (_sdf->HasElement("rosServiceName"))
     this->dataPtr->rosServiceName = _sdf->Get<std::string>("rosServiceName");
 
@@ -110,76 +87,64 @@ void GimbalSmall2dPlugin::Load(physics::ModelPtr _model, sdf::ElementPtr _sdf) {
     this->dataPtr->rosTopicName = _sdf->Get<std::string>("rosTopicName");
 
   if (_sdf->HasElement("vehicleModelName"))
-    this->dataPtr->vehicleModelName =
-        _sdf->Get<std::string>("vehicleModelName");
+    this->dataPtr->vehicleModelName = _sdf->Get<std::string>("vehicleModelName");
 
-  this->dataPtr->tiltJoint =
-      this->dataPtr->model->GetJoint(this->dataPtr->jointName);
-  if (!this->dataPtr->tiltJoint) {
-    std::string scopedJointName =
-        _model->GetScopedName() + "::" + this->dataPtr->jointName;
+  this->dataPtr->tiltJoint = this->dataPtr->model->GetJoint(this->dataPtr->jointName);
+  if (!this->dataPtr->tiltJoint)
+  {
+    std::string scopedJointName = _model->GetScopedName() + "::" + this->dataPtr->jointName;
     this->dataPtr->tiltJoint = this->dataPtr->model->GetJoint(scopedJointName);
   }
-  if (!this->dataPtr->tiltJoint) {
-    gzerr << "GimbalSmall2dPlugin::Load ERROR! Can't find joint: "
-          << this->dataPtr->jointName << endl;
+  if (!this->dataPtr->tiltJoint)
+  {
+    gzerr << "GimbalSmall2dPlugin::Load ERROR! Can't find joint: " << this->dataPtr->jointName << endl;
     return;
   }
 
-  // 🌟 获取带作用域的精确关节名称，用于 JointController
-  this->dataPtr->jointNameScoped = this->dataPtr->tiltJoint->GetScopedName();
-
-  if (!ros::isInitialized()) {
+  if (!ros::isInitialized())
+  {
     int argc = 0;
-    char **argv = NULL;
-    ros::init(argc, argv, "gazebo_gimbal_plugin",
-              ros::init_options::NoSigintHandler);
+    char** argv = NULL;
+    ros::init(argc, argv, "gazebo_gimbal_plugin", ros::init_options::NoSigintHandler);
   }
 
   this->dataPtr->rosNode.reset(new ros::NodeHandle("~"));
 
   ros::AdvertiseServiceOptions aso =
-      ros::AdvertiseServiceOptions::create<rsos_msgs::SetGimbalAngle>(
-          this->dataPtr->rosServiceName,
-          boost::bind(&GimbalSmall2dPluginPrivate::OnServiceCall,
-                      this->dataPtr.get(), _1, _2),
-          ros::VoidPtr(), &this->dataPtr->rosQueue);
+    ros::AdvertiseServiceOptions::create<rsos_msgs::SetGimbalAngle>(
+      this->dataPtr->rosServiceName,
+      boost::bind(&GimbalSmall2dPluginPrivate::OnServiceCall, this->dataPtr.get(), _1, _2),
+      ros::VoidPtr(),
+      &this->dataPtr->rosQueue);
   this->dataPtr->rosSrv = this->dataPtr->rosNode->advertiseService(aso);
 
-  this->dataPtr->rosPub = this->dataPtr->rosNode->advertise<std_msgs::Float64>(
-      this->dataPtr->rosTopicName, 1);
-  this->dataPtr->rosQueueThread = std::thread(
-      std::bind(&GimbalSmall2dPluginPrivate::QueueThread, this->dataPtr.get()));
+  this->dataPtr->rosPub = this->dataPtr->rosNode->advertise<std_msgs::Float64>(this->dataPtr->rosTopicName, 1);
+  this->dataPtr->rosQueueThread = std::thread(std::bind(&GimbalSmall2dPluginPrivate::QueueThread, this->dataPtr.get()));
 }
 
-void GimbalSmall2dPlugin::Init() {
+void GimbalSmall2dPlugin::Init()
+{
   this->dataPtr->lastUpdateTime = this->dataPtr->model->GetWorld()->SimTime();
   this->dataPtr->lastPubTime = this->dataPtr->model->GetWorld()->SimTime();
-
-  // 🌟 核心注入：在内置的 JointController 中注册该云台关节，并赋予一套合理的隐式
-  // PID 参数
-  // 这套参数既够硬（锁死不抖），又合乎物理规律（有最大力矩限制，不伤飞机）
-  // this->dataPtr->model->GetJointController()->AddJoint(
-  //     this->dataPtr->tiltJoint);
-  // gazebo::common::PID safePid(25.0, 0.5, 1.2, 20.0, -20.0, 40.0, -40.0);
-  // this->dataPtr->model->GetJointController()->SetPositionPID(
-  //     this->dataPtr->jointNameScoped, safePid);
 
   this->dataPtr->connections.push_back(event::Events::ConnectWorldUpdateBegin(
       std::bind(&GimbalSmall2dPlugin::OnUpdate, this)));
 }
 
-void GimbalSmall2dPluginPrivate::QueueThread() {
+void GimbalSmall2dPluginPrivate::QueueThread()
+{
   static const double timeout = 0.01;
-  while (this->rosNode->ok()) {
+  while (this->rosNode->ok())
+  {
     this->rosQueue.callAvailable(ros::WallDuration(timeout));
   }
 }
 
-bool GimbalSmall2dPluginPrivate::OnServiceCall(
-    rsos_msgs::SetGimbalAngle::Request &req,
-    rsos_msgs::SetGimbalAngle::Response &res) {
-  if (req.mode != "abs" && req.mode != "body") {
+bool GimbalSmall2dPluginPrivate::OnServiceCall(rsos_msgs::SetGimbalAngle::Request &req,
+                                               rsos_msgs::SetGimbalAngle::Response &res)
+{
+  if (req.mode != "abs" && req.mode != "body")
+  {
     res.success = false;
     res.message = "Error: Invalid mode! Supported modes: 'abs' or 'body'.";
     return true;
@@ -190,8 +155,7 @@ bool GimbalSmall2dPluginPrivate::OnServiceCall(
   this->targetAngleDeg = req.angle;
 
   res.success = true;
-  res.message = "Gimbal target updated to " + to_string(req.angle) + " deg (" +
-                req.mode + ")";
+  res.message = "Gimbal target updated to " + to_string(req.angle) + " deg (" + req.mode + ")";
   return true;
 }
 
@@ -199,12 +163,25 @@ void GimbalSmall2dPlugin::OnUpdate()
 {
   if (!this->dataPtr->tiltJoint) return;
 
-  // 1. 获取机身纯净姿态
-  physics::LinkPtr parentLink = this->dataPtr->tiltJoint->GetParent();
-  if (!parentLink) return;
-  ignition::math::Pose3d parentWorldPose = parentLink->WorldPose();
+  common::Time time = this->dataPtr->model->GetWorld()->SimTime();
+  if (time <= this->dataPtr->lastUpdateTime) return;
 
-  // 2. 读取 ROS 目标角度（输入：0=水平向前，90=垂直向下）
+  // 1. 获取目标飞机模型姿态 (为 abs 模式准备)
+  physics::ModelPtr vehicleModel = this->dataPtr->model; 
+  if (!this->dataPtr->vehicleModelName.empty())
+  {
+    auto foundModel = this->dataPtr->model->GetWorld()->ModelByName(this->dataPtr->vehicleModelName);
+    if (foundModel) vehicleModel = foundModel;
+  }
+
+  // 计算飞机低头弧度（Nose Down 为正）
+  ignition::math::Pose3d worldPose = vehicleModel->WorldPose();
+  ignition::math::Vector3d forwardVec = worldPose.Rot().RotateVector(ignition::math::Vector3d::UnitX);
+  double dronePitchDownRad = atan2(-forwardVec.Z(), sqrt(forwardVec.X()*forwardVec.X() + forwardVec.Y()*forwardVec.Y()));
+
+  // 2. 获取当前状态与目标状态
+  double currentJointRad = this->dataPtr->tiltJoint->Position(0);
+  
   double targetAngleRad = 0.0;
   std::string mode;
   {
@@ -213,42 +190,34 @@ void GimbalSmall2dPlugin::OnUpdate()
     mode = this->dataPtr->currentMode;
   }
 
+  // 3. 计算物理关节应该达到的目标弧度 (0=向前, 1.57=向下)
   double targetJointRad = 0.0;
-
-  if (mode == "body") 
-  {
-    // 机身模式：由于你的关节天生就是 0度=向前，90度=向下，直接赋值
+  if (mode == "body") {
     targetJointRad = targetAngleRad;
-  } 
-  else if (mode == "abs") 
-  {
-    // 绝对大地模式：
-    // Gazebo 标准的 parentWorldPose.Rot().Pitch() 定义是：抬头为正，低头为负
-    // 物理定量分析：
-    // 当输入绝对 90 度（向下）时：
-    // - 如果飞机水平（Pitch=0），云台转动 90 度，垂直向下。
-    // - 如果飞机抬头（Pitch=+10度），机头往上翘，云台相对机身必须【多向下转 10 度】才能保持垂直向下。
-    // 综上所述，逻辑应该是非常纯粹的加法：
-    double vehiclePitchRad = parentWorldPose.Rot().Pitch();
-    targetJointRad = targetAngleRad + vehiclePitchRad;
+  } else if (mode == "abs") {
+    targetJointRad = targetAngleRad - dronePitchDownRad;
   }
 
-  // 3. 强行设定关节物理位置（无 PID，彻底消除突变和打架导致的微小抖动）
-  this->dataPtr->tiltJoint->SetPosition(0, targetJointRad);
+  // 🌟 4. 100% 还原原作者的底层力矩控制闭环算法
+  double dt = (time - this->dataPtr->lastUpdateTime).Double();
+  // 按照原作者公式：error = angle - command
+  double error = currentJointRad - targetJointRad; 
+  double force = this->dataPtr->pid.Update(error, dt);
+  
+  // 施加受限制的微小扭矩
+  this->dataPtr->tiltJoint->SetForce(0, force);
+  this->dataPtr->lastUpdateTime = time;
 
-  // 4. 实时发布状态 (50Hz)
-  common::Time time = this->dataPtr->model->GetWorld()->SimTime();
-  if ((time - this->dataPtr->lastPubTime).Double() >= 0.02) {
+  // 5. 发布符合 FRD 的状态反馈 (50Hz，向下为负)
+  if ((time - this->dataPtr->lastPubTime).Double() >= 0.02)
+  {
     std_msgs::Float64 pitchMsg;
-    double currentJointRad = this->dataPtr->tiltJoint->Position(0);
-    double vehiclePitchRad = parentWorldPose.Rot().Pitch();
-
+    
     if (mode == "body") {
-      pitchMsg.data = -(currentJointRad * 180.0 / M_PI);
+      pitchMsg.data = - (currentJointRad * 180.0 / M_PI);
     } else if (mode == "abs") {
-      // 绝对模式下，反馈当前相对于大地的绝对角度
-      double currentAbsRad = currentJointRad - vehiclePitchRad;
-      pitchMsg.data = -(currentAbsRad * 180.0 / M_PI);
+      double currentAbsDownwardRad = currentJointRad + dronePitchDownRad;
+      pitchMsg.data = - (currentAbsDownwardRad * 180.0 / M_PI);
     }
 
     this->dataPtr->rosPub.publish(pitchMsg);
